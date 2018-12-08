e1d7a9305ad291d53ec2f489f57f4d6e441b6696
Stefan Koch <programming@stefan-koch.name>
2018-12-08 12:55:49 +0100

    restart supervisor to make new conf files available

--- postroot.sh
+++ postroot.sh
@@ -3,4 +3,5 @@ pluginfoldername=$3
 cp /opt/loxberry/data/plugins/$pluginfoldername/supervisor.conf /etc/supervisor/conf.d/texttospeech.conf
 
 supervisorctl reread
+supervisorctl reload
 supervisorctl start texttospeech
